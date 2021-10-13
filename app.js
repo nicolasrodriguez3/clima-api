87ad3b74b359f4d09b3b6f8fc761632432e06d9e
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2021-10-13 16:30:28 -0300

    corrigiendo errores de js

--- app.js
+++ app.js
@@ -1,72 +1,65 @@
-var boton = document.getElementById("btn");
-var botonLimpiar = document.getElementById("btn-clear");
-var principal = document.getElementById("principal");
+const boton = document.getElementById("btn");
+const botonLimpiar = document.getElementById("btn-clear");
+const principal = document.getElementById("principal");
+const icon = document.getElementById("icon1");
+const listaCiudades = document.getElementById("lista-ciudades");
 
-var datos;
+let datos;
+
 boton.addEventListener("click", function () {
 	fetch("https://ws.smn.gob.ar/map_items/weather")
 		.then((data) => data.json())
 		.then((data) => {
-			datos = data;
 			mostrarDatos(datos);
 		});
 });
 
+// obtener los elementos html
+const ciudadNombre = document.querySelector(".ciudad-nombre");
+const temp = document.querySelector(".temperatura-grados");
+const descripcion = document.querySelector(".temperatura-descripcion");
+
 function mostrarDatos(datos) {
-	console.log(datos);
-	var ciudadUsuario = document.getElementById("ciudad").value;
-	console.log(ciudadUsuario);
-
-	//crear los elementos html
-	var section = document.createElement("section");
-	var temp = document.createElement("span");
-	var city = document.createElement("h1");
-	var description = document.createElement("p");
-
-	//modificar atributos
-	section.className = "tarjetas";
-	section.style = 'background: "#FFF"';
-	temp.className = "temp";
-	city.className = "city";
-	description.className = "description";
-
-	var indexCity = -1;
+	// obtenemos la ciudad del usuario
+	const ciudadUsuario = document.getElementById("miCiudad").value;
+	// iteramos la API buscando la ciudad del usuario
 	for (const iterator in datos) {
-		console.log(datos[iterator].name);
 		if (datos[iterator].name == ciudadUsuario) {
-			indexCity = iterator;
-			console.log(indexCity);
-			break;
-		} else {
-			indexCity = null;
+			//modificamos los elementos HTML
+			ciudadNombre.textContent = datos[iterator].name;
+			temp.textContent = datos[iterator].weather.temp + "°";
+			descripcion.textContent = datos[iterator].weather.description;
 		}
 	}
-	if (!indexCity) {
-		section.innerHTML = "<p>" + ciudadUsuario + " no existe</p>";
-	} else {
-		console.log(indexCity);
-
-		//añadir contenido
-		temp.textContent = datos[indexCity].weather.tempDesc;
-		city.textContent = datos[indexCity].name;
-		description.textContent = datos[indexCity].weather.description;
-
-		//crear la tarjeta con los elementos
-		section.appendChild(temp);
-		section.appendChild(city);
-		section.appendChild(description);
-	}
-	//insertar la tarjeta
-	principal.appendChild(section);
 }
 
-//corregir
-botonLimpiar.addEventListener("click", function () {
-	section = document.getElementsByClassName("tarjetas");
-	console.log(section);
-	var nroTarjetas = section.length;
-	for (let i = 0; i < nroTarjetas; i++) {
-		//principal.removeChild(section[i]);
-		section[i].remove();
-	}
+function insertIcon(canva) {
+	const skycons = new Skycons({ color: "white" });
+	skycons.add(canva, Skycons.PARTLY_CLOUDY_DAY);
+	skycons.play();
+}
+
+insertIcon(icon);
+
+//cargar las opciones en la lista de ciudades
+window.addEventListener("load", function () {
+	fetch("https://ws.smn.gob.ar/map_items/weather")
+		.then((data) => data.json())
+		.then((data) => {
+			datos = data;
+			data.forEach((ciudad) => {
+				//crear opciones
+				let opcion = document.createElement("option");
+				opcion.value = ciudad.name;
+				//insertar la ciudad en la lista
+				listaCiudades.appendChild(opcion);
+			});
+			let ciudadRandom = Math.floor(Math.random() * data.length);
+			ciudadNombre.textContent = data[ciudadRandom].name;
+			temp.textContent = data[ciudadRandom].weather.temp;
+			descripcion.textContent = data[ciudadRandom].weather.description;
+		});
 });
+
+switch (datos[1].weather.temp) {
+}
