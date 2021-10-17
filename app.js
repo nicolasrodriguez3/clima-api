2607bb5a846e9977d420c5b8020ebf027c43bc7c
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2021-10-17 20:21:36 -0300

    agregando actualizacion del icono

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const boton = document.getElementById("btn");
 const botonLimpiar = document.getElementById("btn-clear");
 const principal = document.getElementById("principal");
-const icon = document.getElementById("icon1");
+const canva = document.getElementById("icon1");
 const listaCiudades = document.getElementById("lista-ciudades");
 
 let datos;
@@ -29,17 +29,19 @@ function mostrarDatos(datos) {
 			ciudadNombre.textContent = datos[iterator].name;
 			temp.textContent = datos[iterator].weather.temp + "°";
 			descripcion.textContent = datos[iterator].weather.description;
+
+			//cambiamos en icono
+			actualizarIcono(datos[iterator].weather.description);
+			insertIcon(canva, icono);
 		}
 	}
 }
 
-function insertIcon(canva) {
+function insertIcon(canva, icono = Skycons.PARTLY_CLOUDY_DAY) {
 	const skycons = new Skycons({ color: "white" });
-	skycons.add(canva, Skycons.PARTLY_CLOUDY_DAY);
+	skycons.set(canva, icono);
 	skycons.play();
 }
-
-insertIcon(icon);
 
 //cargar las opciones en la lista de ciudades
 window.addEventListener("load", function () {
@@ -58,8 +60,53 @@ window.addEventListener("load", function () {
 			ciudadNombre.textContent = data[ciudadRandom].name;
 			temp.textContent = data[ciudadRandom].weather.temp;
 			descripcion.textContent = data[ciudadRandom].weather.description;
+
+			actualizarIcono(data[ciudadRandom].weather.description);
+			insertIcon(canva, icono);
 		});
 });
 
-switch (datos[1].weather.temp) {
+let icono;
+function actualizarIcono(iconoCiudad) {
+	switch (iconoCiudad) {
+		case "Despejado":
+			icono = Skycons.CLEAR_DAY;
+			break;
+		case "Parcialmente nublado":
+			icono = Skycons.PARTLY_CLOUDY_DAY;
+			break;
+		case "Algo nublado":
+			icono = Skycons.PARTLY_CLOUDY_DAY;
+			break;
+		case "Nublado":
+			icono = Skycons.CLOUDY;
+			break;
+		case "Cubierto":
+			icono = Skycons.CLOUDY;
+			break;
+		case "Cubierto con precipitación a la vista":
+			icono = Skycons.CLOUDY;
+			break;
+		case "Nublado con llovizna":
+			icono = Skycons.RAIN;
+			break;
+		case "Cubierto con tormenta en la hora anterior":
+			icono = Skycons.RAIN;
+			break;
+		case "Nublado con tormenta sin precipitación":
+			icono = Skycons.RAIN;
+			break;
+		case "Cubierto con nevada":
+			icono = Skycons.SNOW;
+			break;
+		case "Cubierto con ventisca baja":
+			icono = Skycons.WIND;
+			break;
+		case "Algo nublado con polvo levantado por viento":
+			icono = Skycons.WIND;
+			break;
+		default:
+			console.log(iconoCiudad);
+			break;
+	}
 }
